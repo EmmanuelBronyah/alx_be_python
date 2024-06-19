58e1f8c1f017859167cbac76ee243c6e8c84f9a2
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-06-19 15:48:15 +0000

    Updated hours_to_seconds.py.

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,4 +1,3 @@
 hours = 2
-seconds_per_hour = 3600
-seconds_in_two_hours = seconds_per_hour * 2
-print(f'2 hour(s) is {seconds_in_two_hours} seconds.')
+seconds = hours * 3600
+print(f'2 hour(s) is {seconds} seconds.')
