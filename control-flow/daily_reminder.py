e5e61d00227a3152a4dcac13b371060f6e24cb93
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-06-29 13:40:33 +0000

    Made modifications.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,21 +2,33 @@ task = input("Enter your task: ")
 
 priority = input("Priority (high/medium/low): ")
 
-time_bound = input("Is it time bound? (yes/no): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
   case "high":
+    reminder = ""
+    
     if time_bound == "yes":
-      print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
+      reminder = f"Reminder: '{task}' is a high priority task that requires immediate attention today!"
+      print(reminder)
     elif time_bound == "no":
-      print(f"Note: '{task}' is a high priority task. Consider completing it when you have free time.")
+      reminder = f"Note: '{task}' is a high priority task. Consider completing it when you have free time."
+      print(reminder)
   case "medium":
+    reminder = ""
+    
     if time_bound == "yes":
-      print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
+      reminder = f"Reminder: '{task}' is a medium priority task that requires immediate attention today!"
+      print(reminder)
     elif time_bound == "no":
-      print(f"Note: '{task}' is a medium priority task. Consider completing it when you have free time.")
+      reminder = f"Note: '{task}' is a medium priority task. Consider completing it when you have free time."
+      print(reminder)
   case "low":
+    reminder = ""
+    
     if time_bound == "yes":
-      print(f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
+      reminder = f"Reminder: '{task}' is a low priority task that requires immediate attention today!"
+      print(reminder)
     elif time_bound == "no":
-      print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
+      reminder = f"Note: '{task}' is a low priority task. Consider completing it when you have free time."
+      print(reminder)
