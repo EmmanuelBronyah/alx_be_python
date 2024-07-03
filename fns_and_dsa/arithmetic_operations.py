35a50a8cbc074821cea30031b79404a274f443ae
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-07-03 06:42:38 +0000

    Created fns_and_dsa and arithmetic_operations file.

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation(num1, num2, operation):
+  match operation:
+    case "add":
+      result = float(num1) + float(num2)
+      return result
+    case "subtract":
+      result = float(num1) - float(num2)
+      return result
+    case "multiply":
+      result = float(num1) * float(num2)
+      return result
+    case "divide":
+      if num2 == 0:
+        result = "Cannot divide by zero."
+        return result
+      else:
+        result = float(num1) / float(num2)
+        return result
