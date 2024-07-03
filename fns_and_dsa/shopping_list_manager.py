342d7d9d47d4a3d98b0b851893a22acb5c2d5115
Emmanuel Bronyah <emmanuelbronyah@yahoo.com>
2024-07-03 08:27:16 +0000

    shopping_list_manager.py.

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,33 @@
+def display_menu():
+  print("Shopping List Manager")
+  print("1. Add Item")
+  print("2. Remove Item")
+  print("3. View List")
+  print("4. Exit")
+
+def main():
+  shopping_list = []
+  while True:
+    display_menu()
+    choice = input("Enter your choice: ")
+    
+    if choice == "1":
+      item_name = input("Provide the item name: ")
+      shopping_list.append(item_name)
+    elif choice == "2":
+      item_name = input("Provide the item name: ")
+      if item_name not in shopping_list:
+        print("Item not found.")
+      else:
+        shopping_list.remove(item_name)
+    elif choice == "3":
+      for item in shopping_list:
+        print(item)
+    elif choice == "4":
+      print("Goodbye!")
+      break
+    else:
+      print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+  main()
